26513092f864c149da10eab7ec70ae7bd56c1215
areeshaak30 <areesha.ak30@gmail.com>
2024-02-05 00:39:39 +0500

    Practice

--- src/pages/Wallet.jsx
+++ src/pages/Wallet.jsx
@@ -8,9 +8,6 @@ export default function Wallet() {
             <main style={{margintop: '58px'}}>
                 <div className="container pt-4">
                    <h1>I am Wallet page and In progress......</h1>
-                   <h1>I am Wallet page and In progress......</h1>
-                   <h1>I am Wallet page and In progress......</h1>
-                   <h1>I am Wallet page and In progress......</h1>
                 </div>
             </main>
     </div>            
