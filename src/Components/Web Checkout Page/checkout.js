e1c2df45808ee6f1335a0b66fa4baa0a3461cdcc
areeshaak30 <areesha.ak30@gmail.com>
2024-02-13 23:39:12 +0500

    Checkout Page Code and Files

---
+++ src/Components/Web Checkout Page/checkout.js
@@ -0,0 +1,9 @@
+function returnToCart() {
+    // Implement functionality to return to cart
+    alert("Returning to cart...");
+}
+
+function continueToShipping() {
+    // Implement functionality to continue to shipping
+    alert("Continuing to shipping...");
+}
