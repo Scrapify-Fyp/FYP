3c67aa0e51d72a7e7c702407dbbddf7cdb1cb345
areeshaak30 <areesha.ak30@gmail.com>
2024-02-18 14:28:15 +0500

    Scrapyard Page Update

---
+++ src/Components/product listing/ScrapProducts.jsx
@@ -0,0 +1,89 @@
+// {
+//     id: 01,
+//     imgSrc: img8, 
+//     Brand: "Handicrafts",
+//     Name: "Crstal Handicrafted Royal Craft",
+//     Ratings: 5,
+//     Price: $1330
+// },
+
+// {
+//     id: 02,
+//     imgSrc: img7,
+//     Brand: "Tealight Candle Holders",
+//     Name: "Decorative Light Glass",
+//     Ratings: 4,
+//     Price: $140
+// },
+
+// {
+//     id: 03,
+//     imgSrc: img2,
+//     Brand: "Handicrafts",
+//     Name: "Pure Craft Hand Bag",
+//     Ratings: 5,
+//     Price: $20
+// },
+
+// {
+//     id: 04,
+//     imgSrc: img6,
+//     Brand: "Candle Holders",
+//     Name: "Candle Holder Candlestick",
+//     Ratings: 4.5,
+//     Price: $134
+// },
+
+// {
+//     id: 05,
+//     imgSrc: scrap-prod-01,
+//     Brand: "Rubbish Revival",
+//     Name: "Sea Glass and Starfish",
+//     Ratings: 5,
+//     Price: $60
+// },
+
+// {
+//     id: 06,
+//     imgSrc: scrap-prod-02,
+//     Brand: "Metal Recyclying",
+//     Name: "Metal Stand for Pens and Pencils",
+//     Ratings: 4,
+//     Price: $175
+// },
+
+// {
+//     id: 07,
+//     imgSrc: scrap-prod-03,
+//     Brand: "Stained Glass",
+//     Name: "Windows Bird Decor",
+//     Ratings: 5,
+//     Price: $35
+// },
+
+// {
+//     id: 08,
+//     imgSrc: scrap-prod-04,
+//     Brand: "Murano Glass",
+//     Name: "Necklace "Moon" in aqua-turquoise",
+//     Ratings: 5,
+//     Price: $45
+// },
+
+// {
+//     id: 09,
+//     imgSrc: img3,
+//     Brand: "Wall Hangings",
+//     Name: "Eye Mandala Set",
+//     Ratings: 4,
+//     Price: $60
+// },
+
+// {
+//     id: 10,
+//     imgSrc: scrap-prod-05,
+//     Brand: "Stained Glass for Window",
+//     Name: "Japanese Crane Unique Suncatcher",
+//     Ratings: 5,
+//     Price: $50
+// }
