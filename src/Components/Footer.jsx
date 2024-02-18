e2293c39ebfb2f96dd0bec0ccb3c4b27e72a71b2
areeshaak30 <areesha.ak30@gmail.com>
2024-02-18 22:26:02 +0500

    Minor Changes Color fonts etc

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -13,7 +13,7 @@ export default function Footer() {
                display:"flex",gap:"20px",paddingTop:"40px"
           }}>
             <div>
-            <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="#fe6401" class="bi bi-geo-alt-fill" viewBox="0 0 16 16">
+            <svg xmlns="http://www.w3.org/2000/svg" width="19" height="19" fill="#52011A" class="bi bi-geo-alt-fill" viewBox="0 0 16 16">
             <path d="M8 16s6-5.686 6-10A6 6 0 0 0 2 6c0 4.314 6 10 6 10m0-7a3 3 0 1 1 0-6 3 3 0 0 1 0 6"/>
           </svg>
             </div>
@@ -23,7 +23,7 @@ export default function Footer() {
                display:"flex",gap:"20px",paddingTop:"7px"
           }}>
             <div>
-            <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="#fe6401" class="bi bi-envelope-fill" viewBox="0 0 16 16">
+            <svg xmlns="http://www.w3.org/2000/svg" width="19" height="19" fill="#52011A" class="bi bi-envelope-fill" viewBox="0 0 16 16">
   <path d="M.05 3.555A2 2 0 0 1 2 2h12a2 2 0 0 1 1.95 1.555L8 8.414zM0 4.697v7.104l5.803-3.558zM6.761 8.83l-6.57 4.027A2 2 0 0 0 2 14h12a2 2 0 0 0 1.808-1.144l-6.57-4.027L8 9.586zm3.436-.586L16 11.801V4.697z"/>
 </svg>
             </div>
@@ -32,7 +32,7 @@ export default function Footer() {
                display:"flex",gap:"20px",paddingTop:"7px"
           }}>
             <div>
-            <svg xmlns="http://www.w3.org/2000/svg"  width="18" height="18" fill="#fe6401" class="bi bi-telephone-fill" viewBox="0 0 16 16">
+            <svg xmlns="http://www.w3.org/2000/svg"  width="19" height="19" fill="#52011A" class="bi bi-telephone-fill" viewBox="0 0 16 16">
   <path fill-rule="evenodd" d="M1.885.511a1.745 1.745 0 0 1 2.61.163L6.29 2.98c.329.423.445.974.315 1.494l-.547 2.19a.68.68 0 0 0 .178.643l2.457 2.457a.68.68 0 0 0 .644.178l2.189-.547a1.75 1.75 0 0 1 1.494.315l2.306 1.794c.829.645.905 1.87.163 2.611l-1.034 1.034c-.74.74-1.846 1.065-2.877.702a18.6 18.6 0 0 1-7.01-4.42 18.6 18.6 0 0 1-4.42-7.009c-.362-1.03-.037-2.137.703-2.877z"/>
 </svg>
             </div>
@@ -44,7 +44,7 @@ export default function Footer() {
           <div className={styles.footerLi}>
             <div>
             <svg
-      fill="#fe6401"
+      fill="#52011A"
       height="11px"
       width="11px"
       version="1.1"
@@ -71,7 +71,7 @@ export default function Footer() {
           <div className={styles.footerLi}>
             <div>
             <svg
-      fill="#fe6401"
+      fill="#52011A"
       height="11px"
       width="11px"
       version="1.1"
@@ -98,7 +98,7 @@ export default function Footer() {
           <div className={styles.footerLi}>
             <div>
             <svg
-      fill="#fe6401"
+      fill="#52011A"
       height="11px"
       width="11px"
       version="1.1"
@@ -125,7 +125,7 @@ export default function Footer() {
           <div className={styles.footerLi}>
             <div>
             <svg
-      fill="#fe6401"
+      fill="#52011A"
       height="11px"
       width="11px"
       version="1.1"
@@ -152,7 +152,7 @@ export default function Footer() {
           <div className={styles.footerLi}>
             <div>
             <svg
-      fill="#fe6401"
+      fill="#52011A"
       height="11px"
       width="11px"
       version="1.1"
@@ -181,7 +181,7 @@ export default function Footer() {
           <div className={styles.footerLi}>
             <div>
             <svg
-      fill="#fe6401"
+      fill="#52011A"
       height="11px"
       width="11px"
       version="1.1"
@@ -208,7 +208,7 @@ export default function Footer() {
           <div className={styles.footerLi}>
             <div>
             <svg
-      fill="#fe6401"
+      fill="#52011A"
       height="11px"
       width="11px"
       version="1.1"
@@ -235,7 +235,7 @@ export default function Footer() {
           <div className={styles.footerLi}>
             <div>
             <svg
-      fill="#fe6401"
+      fill="#52011A"
       height="11px"
       width="11px"
       version="1.1"
@@ -262,7 +262,7 @@ export default function Footer() {
           <div className={styles.footerLi}>
             <div>
             <svg
-      fill="#fe6401"
+      fill="#52011A"
       height="11px"
       width="11px"
       version="1.1"
@@ -289,7 +289,7 @@ export default function Footer() {
           <div className={styles.footerLi}>
             <div>
             <svg
-      fill="#fe6401"
+      fill="#52011A"
       height="11px"
       width="11px"
       version="1.1"
@@ -319,7 +319,9 @@ export default function Footer() {
           <div className={styles.footerSubscribeBtn}>
           <h className="footer-subscribe-btntext"> Subscribe</h>
           </div>
-          <p>Get the latest news & updates</p>
+          <p style={{
+            color: "black", fontSize: "16px"
+          }}> Get the latest news & updates</p>
           <div className={styles.footerIcons}>
            <div>
              <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="#ffffff" class="bi bi-facebook footer-svg-colour" viewBox="0 0 16 16">
